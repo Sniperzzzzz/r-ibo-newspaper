3bf3434008350cbc36a1d062c69a3d19baa888e9
PhytonKing <reachcreditshare@creditshare.com>
2023-07-02 21:05:15 +0200

    dfgh
    fg

--- newspaper/web/models.py
+++ newspaper/web/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# User used for main account login and etc. 
+
+class U(models.Model):
+    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    is_subscribed = models.BooleanField()
+
+# model U is used for account management and subscription to the services. 
 
 # Create your models here.
 class Article(models.Model):
--- newspaper/web/views.py
+++ newspaper/web/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from web.models import *
-# Create your views here.
-def index(request):
-    return render(request, "web/new_layout.html")
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
 
+"""
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "web/login.html")
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return HttpResponseRedirect(reverse("web:index"))
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, "web/login.html", {"Error": "Login did not process through. Please try Again."})
+
+
+# Create your views here.
+@login_required(login_url='/login')
+def index(request):
+    return render(request, "web/index.html", {"posts": Article.objects.all()[0:5]})
+
+"""
+
+#@login_required(login_url='/login')
 def article_view(request, articleID):
     if request.method == "GET":
-        return render(request, "web/new_layout.html", {"site": Article.objects.get(article_id=articleID), "posts": Article.objects.all()[0:5]})
+        #return render(request, "web/new_layout.html", {"site": Article.objects.get(article_id=articleID), "posts": Article.objects.all()[0:5]})
+        us = request.user
+        
+        return JsonResponse({"site": Article.objects.get(article_id=articleID), "posts": Article.objects.all()[0:5]})
